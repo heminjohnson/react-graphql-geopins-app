6b07f1b90efd5d521e8366fbc938dd96d8067965
Hemin Johnson <achayan1992@gmail.com>
2019-11-02 03:36:42 +0100

    feat: place a pin at the users current position

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
-import ReactMapGL, { NavigationControl } from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+import PinIcon from "./PinIcon";
 
 const INITIAL_VIEWPORT = {
   latitude: 37.7577,
@@ -13,12 +15,27 @@ const INITIAL_VIEWPORT = {
 
 const Map = ({ classes }) => {
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPosition] = useState(null);
+
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
+  const getUserPosition = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  };
 
   return (
     <div className={classes.root}>
       <ReactMapGL
         width="100vw"
-        height="calc(100vw - 64px)"
+        height="calc(100vh - 64px)"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         onViewportChange={newViewport => setViewport(newViewport)}
         mapboxApiAccessToken={process.env.REACT_APP_MAP_API_ACCESS_TOKEN}
@@ -29,6 +46,16 @@ const Map = ({ classes }) => {
             onViewportChange={newViewport => setViewport(newViewport)}
           />
         </div>
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
       </ReactMapGL>
     </div>
   );
