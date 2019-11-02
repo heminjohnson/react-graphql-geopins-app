daa8101ae05fca46dea7532107d8f382facb6101
Hemin Johnson <achayan1992@gmail.com>
2019-11-02 03:06:36 +0100

    feat: create and style map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL, { NavigationControl } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vw - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        mapboxApiAccessToken={process.env.REACT_APP_MAP_API_ACCESS_TOKEN}
+        {...viewport}
+      >
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
